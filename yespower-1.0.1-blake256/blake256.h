4537af2371054a3d942b91067938cd8e651da867
zcdk077 <zackynautika@gmail.com>
2025-02-20 10:39:55 +0700

    Update blake256.h

--- yespower-1.0.1-blake256/blake256.h
+++ yespower-1.0.1-blake256/blake256.h
@@ -24,16 +24,16 @@ typedef struct {
 extern "C" {
 #endif
 
-void blake256_init(blake256_ctx *S);
-void blake256_update(blake256_ctx *S, const uint8_t *data, uint64_t datalen);
-void blake256_final(blake256_ctx *S, uint8_t *digest);
+void blake256_init(blake256_ctx *ctx);
+void blake256_update(blake256_ctx *ctx, const uint8_t *data, uint64_t datalen);
+void blake256_final(blake256_ctx *ctx, uint8_t *digest);
 void blake256_hash(uint8_t *out, const uint8_t *in, uint64_t inlen);
 
 /* HMAC functions: */
 
-void hmac_blake256_init(hmac_ctx *S, const uint8_t *_key, uint64_t keylen);
-void hmac_blake256_update(hmac_ctx *S, const uint8_t *data, uint64_t datalen);
-void hmac_blake256_final(hmac_ctx *S, uint8_t *digest);
+void hmac_blake256_init(hmac_ctx *ctx, const uint8_t *_key, uint64_t keylen);
+void hmac_blake256_update(hmac_ctx *ctx, const uint8_t *data, uint64_t datalen);
+void hmac_blake256_final(hmac_ctx *ctx, uint8_t *digest);
 void hmac_blake256_hash(uint8_t *out, const uint8_t *key, uint64_t keylen, const uint8_t *in, uint64_t inlen);
 
 void pbkdf2_blake256(const uint8_t * passwd, size_t passwdlen, const uint8_t * salt,
